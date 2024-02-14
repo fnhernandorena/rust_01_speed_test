a48666e22b3b5f36dbdc00b90291bfb9aced4b79
Francisco Nicolas Hernandorena <fnhernandorena@gmail.com>
2024-02-14 01:21:53 -0300

    speed-test-rust

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Hello, world!");
+    let a: i32 = 2;
+    let b = 3;
+    let c= a+b;
+    let mut d: usize =1;
+
+
+    println!("{c}");
+
+    while d < 10000000 {
+        d +=1;
+        println!("{}",d);
+
+    };
+println!("finish")
+}
